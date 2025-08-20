188ed4ccf239ac475d96c2e3fd995d3f795265f9
Dhirajsah18 <dhirajsah2003@gmail.com>
2025-08-21 00:04:26 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -8,3 +8,16 @@ dotenv.config({
 
 
 connectDB()
+    .then(() => {
+
+        app.on('error', (err) => {
+            console.error("Server error:", err);
+            throw err; // Re-throw the error to be caught by the catch block
+        });
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port: ${process.env.PORT || 8000}`);
+        }
+    })
+    .catch((error) => {
+        console.error("Failed to connect to the MONGODB:", error);       
+    });
