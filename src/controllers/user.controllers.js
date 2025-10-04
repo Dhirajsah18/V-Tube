98924d5edeaaa6c2f30d3680ae744a7687fa7f1f
Dhirajsah18 <dhirajsah2003@gmail.com>
2025-10-04 23:54:21 +0530

    implement getUserChannelProfile aggregation

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -277,6 +277,12 @@ const updateUserAvatar = asyncHandler( async (req, res) => {
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is missing")
     }
+    // delete old image from cloudinary
+    if (req.user?.avatar){
+        const publicId = req.user.avatar.split("/").pop().split(".")[0]
+        await deleteFromCloudinary(publicId)
+    }
+
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     
     if(!avatar){
@@ -303,6 +309,12 @@ const updateUserCoverImage = asyncHandler( async (req, res) => {
     if(!coverImageLocalPath){
         throw new ApiError(400, "Cover file is missing")
     }
+    // delete old image from cloudinary
+    if (req.user?.coverImage){
+        const publicId = req.user.coverImage.split("/").pop().split(".")[0]
+        await deleteFromCloudinary(publicId)
+    }
+
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
     
     if(!coverImage){
@@ -324,6 +336,72 @@ const updateUserCoverImage = asyncHandler( async (req, res) => {
     .json(new ApiResponse(200, user, "Cover image updated successfully"))
 })
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const {username} = req.params
+
+    if (!username?.trim()){
+        throw new ApiError(400, "Username is missing")
+    }
+
+    const channel = await User.aggregate( [
+        {
+            $match: {
+                username: username.toLowerCase()      
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount: {$size: "$subscribers"},
+                channelsSubscribedToCount: {$size: "$subscribedTo"},
+                isSubscribed: {
+                    $cond: {
+                        if:{$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                email: 1,
+                avatar: 1,
+                coverImage: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                createdAt: 1,
+            }
+        }
+    ])
+    if (!channel?.length){
+        throw new ApiError(404, "Channel not found")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "User Channel profile fetched successfully"))
+})
+
+
 export {
     registerUser,
     loginUser,
@@ -333,5 +411,7 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
+
 }
